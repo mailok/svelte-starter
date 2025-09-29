749428ad722bfed92c3bea128ea78fb685d050e5
Angel A. Núñez <anpodio.nunnez@gmail.com>
2025-09-29 01:31:30 -0300

    feat(sidebar): add draft sidebar component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-	import '../app.css';
-	 import { ModeWatcher } from "mode-watcher";
 	import favicon from '$lib/assets/favicon.svg';
+	import { ModeWatcher } from 'mode-watcher';
+	import '../app.css';
 
 	let { children } = $props();
 </script>
@@ -11,4 +11,6 @@
 </svelte:head>
 
 <ModeWatcher />
-{@render children?.()}
+<div class="size-full">
+	{@render children?.()}
+</div>
--- src/routes/app/+layout.svelte
+++ src/routes/app/+layout.svelte
@@ -1,7 +1,39 @@
 <script lang="ts">
+	import AppSidebar from '$lib/components/sidebar/app-sidebar.svelte';
+	import ThemeToggle from '$lib/components/theme-toggle.svelte';
+	import { Button } from '$lib/components/ui/button/index.js';
+	import { Separator } from '$lib/components/ui/separator/index.js';
+	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
 	let { children } = $props();
 </script>
 
-<div class="size-full">
-	{@render children?.()}
-</div>
+<Sidebar.Provider>
+	<AppSidebar />
+	<Sidebar.Inset>
+		<header
+			class="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)"
+		>
+			<div class="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
+				<Sidebar.Trigger class="-ml-1" />
+				<Separator orientation="vertical" class="mx-2 data-[orientation=vertical]:h-4" />
+				<!-- <Breadcrumbs /> -->
+				<div class="ml-auto flex items-center gap-2">
+					<Button variant="ghost" size="sm" class="hidden sm:flex">
+						<a
+							href="https://github.com/mailok/svelte-starter"
+							rel="noopener noreferrer"
+							target="_blank"
+							class="dark:text-foreground"
+						>
+							GitHub
+						</a>
+					</Button>
+					<ThemeToggle />
+				</div>
+			</div>
+		</header>
+		<div class="size-full">
+			{@render children?.()}
+		</div>
+	</Sidebar.Inset>
+</Sidebar.Provider>
