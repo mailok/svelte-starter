5add5ac2b46da3e1adef963c28393977a9d14e5c
Angel A. Núñez <anpodio.nunnez@gmail.com>
2025-09-28 23:50:12 -0300

    feat: setup shadcn-svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="flex flex-col items-center justify-center h-screen">
+	<h1 class="text-4xl font-bold text-destructive">Welcome</h1>
+</div>
+
