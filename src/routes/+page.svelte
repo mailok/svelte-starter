c91d522e11c839fb1d3a2e00a3698d4f430611fd
Angel A. Núñez <anpodio.nunnez@gmail.com>
2025-09-29 00:05:16 -0300

    feat(ui): add most commonly used ui components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,9 @@
+<script>
+	import ThemeToggle from "@/components/theme-toggle.svelte";
+
+</script>
 <div class="flex flex-col items-center justify-center h-screen">
 	<h1 class="text-4xl font-bold text-destructive">Welcome</h1>
+	<ThemeToggle />
 </div>
 
