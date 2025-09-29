c39a6a7debd53ffda0d54640262367833abac657
Angel A. Núñez <anpodio.nunnez@gmail.com>
2025-09-29 00:46:41 -0300

    chore(vscode): add Prettier configuration for consistent code formatting

--- src/routes/app/+layout.svelte
+++ src/routes/app/+layout.svelte
@@ -1,9 +1,7 @@
 <script lang="ts">
-
 	let { children } = $props();
 </script>
 
-<div class="flex flex-col items-center justify-center h-screen border-1 border-red-500 border-dashed">
-{@render children?.()}
+<div class="size-full">
+	{@render children?.()}
 </div>
-
